936632eb17e91e87df002e469b1ba07d67075b81
Piotr Jarosz <piotr.jarosz@icoqu.com>
2019-10-21 23:39:18 +0200

    Reset password functionality

--- app/models.py
+++ app/models.py
@@ -1,8 +1,10 @@
-from datetime import datetime
+from app import app, db, login
 from flask_login import UserMixin
-from app import db, login
-from werkzeug.security import generate_password_hash, check_password_hash
+from datetime import datetime
+from time import time
 from hashlib import md5
+from werkzeug.security import generate_password_hash, check_password_hash
+import jwt
 
 followers = db.Table('followers',
     db.Column('follower_id', db.Integer, db.ForeignKey('user.id')),
@@ -56,7 +58,20 @@ class User(UserMixin, db.Model):
     def __repr__(self):
         return '<User {}>'.format(self.username)
 
+    def get_reset_password_token(self, expires_in=600):
+        return jwt.encode(
+            {'reset_password': self.id, 'exp': time() + expires_in},
+            app.config['SECRET_KEY'], algorithm='HS256').decode('utf-8')
 
+    @staticmethod
+    def verify_reset_password_token(token):
+        try:
+            id = jwt.decode(token, app.config['SECRET_KEY'],
+                            algorithms=['HS256'])['reset_password']
+        except:
+            return
+        return User.query.get(id)
+        
 class Post(db.Model):
     id = db.Column(db.Integer, primary_key=True)
     body = db.Column(db.String(140))
@@ -69,3 +84,4 @@ class Post(db.Model):
 @login.user_loader
 def load_user(id):
     return User.query.get(int(id))
+
