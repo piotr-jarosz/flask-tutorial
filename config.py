c2aa005d57f748bc78c103de1f7d908a3528a37c
Piotr Jarosz <piotr.jarosz@icoqu.com>
2019-10-21 22:29:08 +0200

    PAgination

--- config.py
+++ config.py
@@ -11,4 +11,5 @@ class Config(object):
     MAIL_USERNAME = os.environ.get('MAIL_USERNAME')
     MAIL_PASSWORD = os.environ.get('MAIL_PASSWORD')
     ADMINS = ['errors@icoqu.com']
+    POSTS_PER_PAGE = 3
     
