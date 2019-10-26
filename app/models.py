cd21917fd6ab7e23eac6bb6848410bb059ea0be8
Piotr Jarosz <piotr.jarosz@icoqu.com>
2019-10-27 00:13:44 +0200

    notifications

--- app/models.py
+++ app/models.py
@@ -9,6 +9,7 @@ from time import time
 from hashlib import md5
 from werkzeug.security import generate_password_hash, check_password_hash
 import jwt
+import json
 
 followers = db.Table('followers',
     db.Column('follower_id', db.Integer, db.ForeignKey('user.id')),
@@ -77,6 +78,14 @@ class User(UserMixin, db.Model):
                                         foreign_keys='Message.recipient_id',
                                         backref='recipient', lazy='dynamic')
     last_message_read_time = db.Column(db.DateTime)
+    notifications = db.relationship('Notification', backref='user',
+                                    lazy='dynamic')
+
+    def add_notification(self, name, data):
+        self.notifications.filter_by(name=name).delete()
+        n = Notification(name=name, payload_json=json.dumps(data), user=self)
+        db.session.add(n)
+        return n
 
     def set_password(self, password):
         self.password_hash = generate_password_hash(password)
@@ -156,3 +165,12 @@ class Message(db.Model):
     def __repr__(self):
         return '<Message {}>'.format(self.body)
 
+class Notification(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    name = db.Column(db.String(128), index=True)
+    user_id = db.Column(db.Integer, db.ForeignKey('user.id'))
+    timestamp = db.Column(db.Float, index=True, default=time)
+    payload_json = db.Column(db.Text)
+
+    def get_data(self):
+        return json.loads(str(self.payload_json))
